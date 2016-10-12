3bc3008470d581d8f355ae80f2161f974688e387
mehmet zantur <mehmetzantur@gmail.com>
2016-10-12 16:27:58 +0300

    Camera Testing in First Running

---
+++ EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EmguCv.FaceRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCamera = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCamera
+            // 
+            this.picCamera.Location = new System.Drawing.Point(12, 41);
+            this.picCamera.Name = "picCamera";
+            this.picCamera.Size = new System.Drawing.Size(553, 413);
+            this.picCamera.TabIndex = 0;
+            this.picCamera.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 466);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picCamera);
+            this.Name = "Form1";
+            this.Text = "Face Recognition Tests";
+            ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCamera;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmguCv.FaceRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //declaring global variables
+        private Capture capture;        //takes images from camera as image frames
+        private bool captureInProgress; // checks if capture is executing
+
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame().ToImage<Bgr, Byte>();  //line 1
+            picCamera.Image = ImageFrame.Bitmap;       //line 2
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            #region if capture is not created, create it now
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    btnStart.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+    }
+}
