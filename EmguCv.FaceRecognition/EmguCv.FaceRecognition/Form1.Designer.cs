fb45ec1c0b85c452ccf013d3ac79b924a96a60ed
mehmet zantur <mehmetzantur@gmail.com>
2016-10-15 18:05:34 +0300

    getting mini image in rect and save label and img to folder

--- EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.Designer.cs
+++ EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.Designer.cs
@@ -30,14 +30,22 @@
         {
             this.picCamera = new System.Windows.Forms.PictureBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.picCaptured = new System.Windows.Forms.PictureBox();
+            this.btnCaptured = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCaptured)).BeginInit();
             this.SuspendLayout();
             // 
             // picCamera
             // 
             this.picCamera.Location = new System.Drawing.Point(12, 41);
             this.picCamera.Name = "picCamera";
-            this.picCamera.Size = new System.Drawing.Size(1227, 755);
+            this.picCamera.Size = new System.Drawing.Size(571, 453);
             this.picCamera.TabIndex = 0;
             this.picCamera.TabStop = false;
             // 
@@ -51,17 +59,88 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // picCaptured
+            // 
+            this.picCaptured.Location = new System.Drawing.Point(684, 41);
+            this.picCaptured.Name = "picCaptured";
+            this.picCaptured.Size = new System.Drawing.Size(555, 453);
+            this.picCaptured.TabIndex = 2;
+            this.picCaptured.TabStop = false;
+            // 
+            // btnCaptured
+            // 
+            this.btnCaptured.Location = new System.Drawing.Point(93, 12);
+            this.btnCaptured.Name = "btnCaptured";
+            this.btnCaptured.Size = new System.Drawing.Size(75, 23);
+            this.btnCaptured.TabIndex = 3;
+            this.btnCaptured.Text = "Capture";
+            this.btnCaptured.UseVisualStyleBackColor = true;
+            this.btnCaptured.Click += new System.EventHandler(this.btnCaptured_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(918, 542);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(174, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save Img";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 637);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(825, 637);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(983, 637);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1251, 808);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCaptured);
+            this.Controls.Add(this.picCaptured);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.picCamera);
             this.Name = "Form1";
             this.Text = "Face Recognition Tests";
             ((System.ComponentModel.ISupportInitialize)(this.picCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCaptured)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +148,13 @@
 
         private System.Windows.Forms.PictureBox picCamera;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox picCaptured;
+        private System.Windows.Forms.Button btnCaptured;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.cs
+++ EmguCv.FaceRecognition/EmguCv.FaceRecognition/Form1.cs
@@ -12,6 +12,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FaceDetection;
+using System.IO;
+using Emgu.CV.Face;
 
 namespace EmguCv.FaceRecognition
 {
@@ -20,6 +22,28 @@ namespace EmguCv.FaceRecognition
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                //Load of previus trainned faces and labels for each image
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels + 1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/face/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Nothing in binary database, please add at least a face(Simply train the prototype with the Add Face Button).", "Triained faces load", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         //declaring global variables
@@ -27,12 +51,21 @@ namespace EmguCv.FaceRecognition
         private bool captureInProgress; // checks if capture is executing
         private Mat myMatImage;
         private CascadeClassifier haar = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        
+
+        Image<Gray, byte> result, TrainedFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
 
         Image<Bgr, Byte> myImageFrame;
         
         private void ProcessFrame(object sender, EventArgs arg)
         {
             myImageFrame = capture.QueryFrame().ToImage<Bgr, Byte>();  //line 1
+            
             myMatImage = myImageFrame.Mat;
             picCamera.Image = myImageFrame.Bitmap; //line 2
             
@@ -54,9 +87,13 @@ namespace EmguCv.FaceRecognition
             List<Rectangle> eyes = new List<Rectangle>();
             DetectFace.Detect(myMatImage, "haarcascade_frontalface_default.xml", faces, tryUseCuda, out detectionTime);
             foreach (Rectangle face in faces)
+            {
                 CvInvoke.Rectangle(myMatImage, face, new Bgr(Color.Red).MCvScalar, 2);
-            foreach (Rectangle eye in eyes)
-                CvInvoke.Rectangle(myMatImage, eye, new Bgr(Color.Blue).MCvScalar, 2);
+                myImageFrame.ROI = face;
+                
+            }
+                
+            
 
 
             //using (var imageFrame = myImageFrame)
@@ -117,5 +154,63 @@ namespace EmguCv.FaceRecognition
 
 
         }
+
+        private void btnCaptured_Click(object sender, EventArgs e)
+        {
+           
+            
+            Image<Gray, Byte> grayed = new Image<Gray, byte>(myImageFrame.Resize(320, 240, Inter.Cubic).Bitmap);
+            
+            picCaptured.Image = grayed.Bitmap;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Trained face counter
+                ContTrain++;
+
+                Image<Gray, Byte> grayed = new Image<Gray, byte>(myImageFrame.Resize(320, 240, Inter.Cubic).Bitmap);
+                TrainedFace = grayed.Resize(100, 100, Inter.Cubic);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+                picCaptured.Image = TrainedFace.Bitmap;
+
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //Write the labels of triained faces in a file text for further load
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()[i - 1] + "%");
+                }
+
+                MessageBox.Show(textBox1.Text + "´s face detected and added :)", "Training OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+
+
+
+        
+
+
+
+
+        
+
+
+
     }
 }
+
+
+
+
+
